16a0edd058d1f12acf9ee7e17b403b4a9340bc81
Didgeridoo_Dave <malermeister-lehnartz@gmx.de>
2025-05-30 18:47:18 +0200

    created routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UserComponent } from './pages/user/user.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainContentComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
